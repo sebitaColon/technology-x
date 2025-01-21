8b077e5d39346ad6ba54d0e8701e17d6e0ac6831
sebitaDev <sebita03082003@gmail.com>
2025-01-21 17:45:42 -0300

    Configurar Next.js con Tailwind CSS

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-600 text-white">
+      <h1 className="text-4xl font-bold">¡Proyecto listo con Next.js y Tailwind CSS!</h1>
+    </div>
+  );
+}
